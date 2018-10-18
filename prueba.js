c4694dc2955029937a7f9ed5e9ec280b972b4978
Sergio Bueno <sergiopatohumanes@gmail.com>
2018-10-18 17:56:51 +0200

    Modificaciones pruebas

--- prueba.js
+++ prueba.js
@@ -1,28 +1,84 @@
-function prueba(){
+function informativa(){
 
 	const endpointUrl = 'https://query.wikidata.org/sparql',
       sparqlQuery = `SELECT ?Agaricales ?AgaricalesLabel ?comestibilidad ?comestibilidadLabel ?imagen WHERE {
-  		SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
- 		?Agaricales (wdt:P171/wdt:P171/wdt:P171/wdt:P171/wdt:P171) wd:Q27720.
-  		?Agaricales wdt:P789 ?comestibilidad.
- 		?Agaricales wdt:P18 ?imagen.
-		}`,
-	      fullUrl = endpointUrl + '?query=' + encodeURIComponent( sparqlQuery ),
-	      headers = { 'Accept': 'application/sparql-results+json' };
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+  ?Agaricales (wdt:P171/wdt:P171/wdt:P171/wdt:P171/wdt:P171) wd:Q27720.
+  ?Agaricales wdt:P789 ?comestibilidad.
+  ?Agaricales wdt:P18 ?imagen.
+  
+}
+LIMIT 5`,
+      fullUrl = endpointUrl + '?query=' + encodeURIComponent( sparqlQuery ),
+      headers = { 'Accept': 'application/sparql-results+json' };
 
-	fetch( fullUrl, { headers } ).then( body => body.json() ).then( json => {
-	    const { head: { vars }, results } = json;
-	    for ( const result of results.bindings ) {
-	    	console.log(result);
-	    	num = 0;
-	        for ( const variable of vars ) {
-	        	num++;
-	        	console.log(num);
-	            //console.log( '%s: %o', variable, result[variable] );
-	        }
-	        console.log( '---' );
-	    }
-	} );
+fetch( fullUrl, { headers } ).then( body => body.json() ).then( json => {
+    const { head: { vars }, results } = json;
+    console.log(results.bindings);
 
+    var result = results.bindings[0].Agaricales.value;
+    console.log(result);
+
+    var numero = 0;
+    for ( const resultado of results.bindings){
+    	console.log(resultado);
+    }
+    /*for ( const result of results.bindings ) {
+    	console.log(result);
+        for ( const variable of vars ) {
+
+        	console.log( '- ' + '%s: %o', variable, result[variable] );
+
+    		var imagen = '<img src=' + JSON.stringify(result[variable].value) + ' alt="asdf"></img>';
+    		document.getElementById("imagen").innerHTML = imagen;
+    		
+
+    		console.log("ELEMENTO IMAGEN: " +imagen);
+    		
+    		console.log("ENLACE: "+JSON.stringify(result[variable].value));
+            
+        }
+        console.log( '---' );
+    }
+    */
+} );
+
+
+
+}
+
+
+function prueba(){
+const endpointUrl = 'https://query.wikidata.org/sparql',
+      sparqlQuery = `SELECT ?Agaricales ?AgaricalesLabel ?comestibilidad ?comestibilidadLabel ?imagen WHERE {
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+  ?Agaricales (wdt:P171/wdt:P171/wdt:P171/wdt:P171/wdt:P171) wd:Q27720.
+  ?Agaricales wdt:P789 ?comestibilidad.
+  ?Agaricales wdt:P18 ?imagen.
+}
+LIMIT 5
+`,
+      fullUrl = endpointUrl + '?query=' + encodeURIComponent( sparqlQuery ),
+      headers = { 'Accept': 'application/sparql-results+json' };
+
+fetch( fullUrl, { headers } ).then( body => body.json() ).then( json => {
+    const { head: { vars }, results } = json;
+    console.log(results.bindings);
+    var n_results = 0;
+    
+    var html = "";
+    for ( const result of results.bindings ) {
+
+    	html += 
+    	'<article>' +
+    	'<p>' + result.AgaricalesLabel.value + '</p>' +
+    	'<img src=' + JSON.stringify(result.imagen.value) + ' width="50" alt="asdf"></img>' +
+    	'</article>'
+
+    }
+
+    document.getElementById("resultados").innerHTML = html;
+
+} );
 
 }
